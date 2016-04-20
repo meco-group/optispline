2ac6b55539742bc4a84aee8eab620a643580d97d
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-04-20 13:56:38 +0200

    issue #2: unittests -- adding valgrind tests

--- tests/test_create_bspline.py
+++ tests/test_create_bspline.py
@@ -1,6 +1,13 @@
 import sys
 sys.path.insert(0, '../swig')
 sys.path.insert(0, '/home/erik/Documents/casadi3.0')
+import os
+
+# Boolean flag to indicate if we run in valgrind
+# To speed up valgrind, you may choose to put
+# expensive computations within 'if not valgrind'
+valgrind = int(os.environ.get("VALGRIND",'0'))
+
 import Basis
 
 print 'MonomialeBasis'
