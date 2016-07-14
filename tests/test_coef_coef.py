d5ad9f77ec613a7004e9ebb278aaae2618252c22
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-07-14 12:30:38 +0200

    tests

---
+++ tests/test_coef_coef.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+sys.path.insert(0, '../swig')
+sys.path.insert(0, '/home/erik/Documents/casadi3.0')
+import os
+import unittest
+
+# Boolean flag to indicate if we run in valgrind
+# To speed up valgrind, you may choose to put
+# expensive computations within 'if not valgrind'
+valgrind = int(os.environ.get("VALGRIND",'0'))
+
+import Basis
+from casadi import *
+
+
+d = DM([[1,2],[3,4]])
+print(d)
+t = Basis.DTensor(DM([[1,2],[3,4]]),[2,2])
+print(t)
+c = Basis.Coefficient(t)
+
+print(c)
---
+++ tests/test_function_function.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+sys.path.insert(0, '../swig')
+sys.path.insert(0, '/home/erik/Documents/casadi3.0')
+import os
+import unittest
+
+# Boolean flag to indicate if we run in valgrind
+# To speed up valgrind, you may choose to put
+# expensive computations within 'if not valgrind'
+valgrind = int(os.environ.get("VALGRIND",'0'))
+
+import Basis
+from casadi import *
+
+b1 = Basis.MonomialeBasis(1)
+b2 = Basis.MonomialeBasis(1)
+b =  Basis. Basis()
+b.addBasis(b1)
+b.addBasis(b2)
+d = DM([[1,2],[3,4]])
+print(d)
+t = Basis.DTensor(DM([[1,2],[3,4]]),[2,2])
+print(t)
+c = Basis.Coefficient(t)
+print(c)
+f = Basis.Function(b, c)
+print(f)
+print(f.basis)
+print(f.coef)
+print(f.coef.data)
+print(f([2,1]))
