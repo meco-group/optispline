ad2903decff58db05da1f84e93f880bbd47aabd5
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-07-18 11:30:47 +0200

    polynomial init fix

--- src/Function/Polynomial.cpp
+++ src/Function/Polynomial.cpp
@@ -7,7 +7,7 @@ namespace spline{
     Polynomial::Polynomial (const std::vector< double >& coef) : 
 // TODO static_cast to supress the waring : narrowing conversion of ‘(& coef)->std::vector<_Tp, _Alloc>::size<double, std::allocator<double> >()’ from ‘std::vector<double>::size_type {aka long unsigned int}’
         // Function(MonomialeBasis((coef.size())-1),
-        Function(Basis(), Coefficient(coef)) {
+        Function(MonomialeBasis(coef.size()-1), Coefficient(coef)) {
     }
 									
     // Polynomial::Polynomial (const std::vector< double >& coef) { 
