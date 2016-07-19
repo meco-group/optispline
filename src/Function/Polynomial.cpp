82b248568e90c042b203768418457f30e98f3a23
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-07-19 11:26:14 +0200

    Polynomial constructors

--- src/Function/Polynomial.cpp
+++ src/Function/Polynomial.cpp
@@ -5,11 +5,17 @@
 namespace spline{
 
     Polynomial::Polynomial (const std::vector< double >& coef) : 
-// TODO static_cast to supress the waring : narrowing conversion of ‘(& coef)->std::vector<_Tp, _Alloc>::size<double, std::allocator<double> >()’ from ‘std::vector<double>::size_type {aka long unsigned int}’
-        // Function(MonomialeBasis((coef.size())-1),
         Function(MonomialeBasis(coef.size()-1), Coefficient(coef)) {
     }
+
+    Polynomial::Polynomial(const std::vector< double >& coef, const Argument argument) : 
+        Function(MonomialeBasis(coef.size()-1, argument), Coefficient(coef)) {
+    }
+
 									
+    Polynomial::Polynomial(const std::vector< double >& coef, const std::string& argumentName) :
+        Function(MonomialeBasis(coef.size()-1, Argument(argumentName)), Coefficient(coef)) {
+    }
     // Polynomial::Polynomial (const std::vector< double >& coef) { 
     //     basis =  MonomialeBasis(static_cast<int>(coef.size())-1);
     //     coef =  Coefficient(new AnyTensor(coef,{static_cast<int>(coef.size()),1,1}))) ;
