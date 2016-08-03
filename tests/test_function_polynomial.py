cbb7816ec5a811be1053b28c9438681d74316a49
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-08-03 16:33:08 +0200

    more tests

--- tests/test_function_polynomial.py
+++ tests/test_function_polynomial.py
@@ -3,7 +3,6 @@ import sys
 sys.path.insert(0, '../swig')
 sys.path.insert(0, '/home/erik/Documents/casadi3.0')
 import os
-import unittest
 
 # Boolean flag to indicate if we run in valgrind
 # To speed up valgrind, you may choose to put
@@ -13,7 +12,32 @@ valgrind = int(os.environ.get("VALGRIND",'0'))
 import Basis
 from casadi import *
 
-f = Basis.Polynomial([1,0,2])
+basis_x =  Basis.MonomialeBasis(2)
+basis_x.setArgument('x')
+basis_y =  Basis.MonomialeBasis(1)
+basis_y.setArgument('y')
+
+basis_xy = basis_x + basis_y
+# print(basis_xy)
+# print(list(basis_xy.getSubBasis())) 
+# print(basis_x([0]).data())    
+# print(basis_y([2]).data())    
+# print(basis_xy([0,2]).data())    
+d = DM([[0,0,0],[0,0,1]])
+# print(d)
+t = Basis.DTensor(d,[3,2,1,1])
+# print(t)
+c = Basis.Coefficient(t)
+# print(c)
+x2y = Basis.Function(basis_xy,c)
+
+print(x2y([1,2]).data())
+print(x2y([2,1]).data())
+
+y2 =  Basis.Polynomial([0,0,1],'y')
+
+print(y2([2]).data())
+print(y2([3]).data())
 # print(f)
 # print(f.basis)
 # print(f.coef)
@@ -32,32 +56,54 @@ r = f([2])
 assert(r.data()==9)
 
 x = Basis.Argument('x')
-g = Basis.Polynomial([0,2],x)
+
+b =  Basis.MonomialeBasis(2)
+c =  Basis.MonomialeBasis(1)
+c2 =  Basis.MonomialeBasis(3,x)
+
+d = c + b
+print(d)
+print(list(d.getSubBasis()))
+d2 = c2 + b
+print(d2)
+print(list(d2.getSubBasis()))
+
+d = DM([[1,0,2]])
+t = Basis.DTensor(d,[3,1,1])
+coef = Basis.Coefficient(t)
+
+
+g = Basis.Polynomial([0,2])
+g2 = Basis.Polynomial([0,2],x)
 h = Basis.Polynomial([1,0,1],'y')
-print(x)
-print(g)
-print(h)
+
+h = Basis.Function(b, coef)
+#print(x)
+#print(g)
+#print(h)
 # print(g.getArgument())
-print(h([0]).data())
-print(h([1]).data())
-print(h([2]).data())
-print(h.basis)
+#print(h([0]).data())
+#print(h([1]).data())
+#print(h([2]).data())
+#print(h.basis)
 print(h.getBasis())
 print(g.getBasis())
 print(h.basis.getArgument())
 print(g.basis.getArgument())
-print('--------------')
+#print('--------------')
 b_ = g.getBasis() + h.getBasis()
 print(b_)
 print(list(b_.getSubBasis()))
 print('--------------')
 s = g + h
+print(s)
+print(list(s.getBasis().getSubBasis()))
 print(g([1]).data())
 print(h([2]).data())
 print(s)
 b = s.getBasis()
 print(b)
 print(list(b.getSubBasis()))
-print(b([1,2]).data())
+#print(b([1,2]).data())
 
 # print(s([1,2]).data())
---
+++ tests/test_function_polynomial_simple.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import sys
+sys.path.insert(0, '../swig')
+sys.path.insert(0, '/home/erik/Documents/casadi3.0')
+import os
+import unittest
+
+# Boolean flag to indicate if we run in valgrind
+# To speed up valgrind, you may choose to put
+# expensive computations within 'if not valgrind'
+valgrind = int(os.environ.get("VALGRIND",'0'))
+
+import Basis
+from casadi import *
+
+# x = Basis.Argument('x')
+
+b =  Basis.MonomialeBasis(2)
+c =  Basis.MonomialeBasis(1)
+c.setArgument('x')
+# d = c + b
+# print(d)
+# print(list(d.getSubBasis()))
+
+d = DM([[1,0,2]])
+t = Basis.DTensor(d,[3,1,1])
+coef1 = Basis.Coefficient(t)
+
+d = DM([[1,2]])
+t = Basis.DTensor(d,[2,1,1])
+coef2 = Basis.Coefficient(t)
+
+
+f = Basis.Function(b, coef1)
+g = Basis.Function(c, coef2)
+
+# print(f)
+# print(g)
+# print(b)
+# print(c)
+# print(f.getBasis())
+# print(g.getBasis())
+# print('f ' + str(list(f.getBasis().getSubBasis())))
+# print('g ' + str(list(g.getBasis().getSubBasis())))
+# print('--------------')
+b_ = b+c
+print(b_)
+print(list(b_.getSubBasis()))
+b_ = b+g.basis
+print(b_)
+print(list(b_.getSubBasis()))
+b_ = g.getBasis() + f.getBasis()
+print(b_)
+print(list(b_.getSubBasis()))
+print('--------------')
+print('b : ' + str(b))
+print('  : ' + str(f.getBasis()))
+print('c : ' + str(c))
+print('  : ' + str(g.getBasis()))
+print(b == b)
+print(b == f.getBasis())
+print(list(b.getSubBasis()))
+print(list(f.getBasis().getSubBasis()))
+print('b : ' + str(b.getDegree()))
+print('  : ' + str(f.getBasis().getDegree()))
+# s = g + f
+# print(s)
+# print(list(s.getBasis().getSubBasis()))
+# print(g([1]).data())
+# print(h([2]).data())
+# print(s)
+# b = s.getBasis()
+# print(b)
+# print(list(b.getSubBasis()))
+# #print(b([1,2]).data())
+#
+# # print(s([1,2]).data())
