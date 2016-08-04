3e95ac92234430c0de46da49806663ab11ee1d04
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-08-04 17:15:31 +0200

    refenece in constructor function

--- src/Function/Polynomial.cpp
+++ src/Function/Polynomial.cpp
@@ -5,20 +5,14 @@
 namespace spline{
 
     Polynomial::Polynomial (const std::vector< double >& coef) : 
-        Function(MonomialeBasis(coef.size()-1), Coefficient(coef)) {
+        Function(*(new MonomialeBasis(coef.size()-1)), Coefficient(coef)) {
     }
 
     Polynomial::Polynomial(const std::vector< double >& coef, const Argument argument) : 
-        Function(MonomialeBasis(coef.size()-1, argument), Coefficient(coef)) {
+        Function(*(new MonomialeBasis(coef.size()-1, argument)), Coefficient(coef)) {
     }
 
-									
     Polynomial::Polynomial(const std::vector< double >& coef, const std::string& argumentName) :
-        Function(MonomialeBasis(coef.size()-1, Argument(argumentName)), Coefficient(coef)) {
+        Function(*(new MonomialeBasis(coef.size()-1, Argument(argumentName))), Coefficient(coef)) {
     }
-    // Polynomial::Polynomial (const std::vector< double >& coef) { 
-    //     basis =  MonomialeBasis(static_cast<int>(coef.size())-1);
-    //     coef =  Coefficient(new AnyTensor(coef,{static_cast<int>(coef.size()),1,1}))) ;
-    // }
-		
 } // namespace spline
