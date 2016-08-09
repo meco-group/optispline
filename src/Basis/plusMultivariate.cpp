6a9f5b979794591f90768c2c6b74511cbb32bdc0
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-08-09 20:50:47 +0200

    refactored

--- src/Basis/plusMultivariate.cpp
+++ src/Basis/plusMultivariate.cpp
@@ -2,47 +2,20 @@
 
 namespace spline{
 
-    Basis plusMultivariate(const Basis& b1, const MonomialeBasis& b2){
-        // Basis plusMultivariateUnivariate(const Basis& b1, BasisT b2){
-        Basis returnBasis = Basis();
-        bool matchingArguments = false;
-
-        // for (int i = 0; i < b1.getSubBasis().size(); ++i) {
-        // auto subBasis = b1.getSubBasis()[i];  
-        for (Basis & subBasis : b1.getSubBasis()) {
-            if(subBasis.getArgument() == b2.getArgument()){
-                BasisNode* w = subBasis.get();
-                if(dynamic_cast<MonomialeBasisNode*>(w)){
-                    std::cout << "if cast" << std::endl;
-                    Basis sum = b2 + subBasis.get()->shared_from_this<MonomialeBasis>();
-                    sum.setArgument(subBasis.getArgument());
-                    returnBasis.addBasis(sum);
-                }
-                // returnBasis.addBasis(b2);
-                matchingArguments = true;
-            }else{
-                returnBasis.addBasis(subBasis);
-            }
-        }
-
-        if(! matchingArguments){
-            returnBasis.addBasis(b1);
-        }
-        return returnBasis;
-    }
-
     Basis plusMultivariate(const Basis& b1, const Basis& b2){
         Basis returnBasis = b1;
 
         for (Basis & subBasis : b2.getSubBasis()) {
             BasisNode* w = subBasis.get();
             if(dynamic_cast<MonomialeBasisNode*>(w)){
-                returnBasis = returnBasis + subBasis.get()->shared_from_this<MonomialeBasis>();
+                returnBasis = returnBasis + w->shared_from_this<MonomialeBasis>();
             }
 
-            // returnBasis = returnBasis + subBasis;
+            if(dynamic_cast<BSplineBasisNode*>(w)){
+                returnBasis = returnBasis + w->shared_from_this<BSplineBasis>();
+            }
         }
 
         return returnBasis;
     }
-    }  // namespace spline
+}  // namespace spline
--- src/Basis/plusMultivariate.h
+++ src/Basis/plusMultivariate.h
@@ -3,44 +3,46 @@
 
 #include "../Function/Argument.h"
 #include "Basis.h"
-// #include "BSplineBasis.h"
+#include "BSplineBasis.h"
 #include "MonomialeBasis.h"
 // #include "plusUnivariate.h"
 namespace spline{
 
-    // // #ifndef SWIG
-    // template< class BasisT>
-    //     Basis plusMultivariate(const Basis& b1, BasisT b2){
-    //     // Basis plusMultivariateUnivariate(const Basis& b1, BasisT b2){
-    //         Basis returnBasis = Basis();
-    //         bool matchingArguments = false;
-    //
-    //         // for (int i = 0; i < b1.getSubBasis().size(); ++i) {
-    //         // auto subBasis = b1.getSubBasis()[i];  
-    //         for (Basis & subBasis : b1.getSubBasis()) {
-    //             if(subBasis.getArgument() == b2->getArgument()){
-    //                 Basis sum = b2 + subBasis;
-    //                 // Basis sum = subBasis->operator+(b2);
-    //                 // sum.setArgument(subBasis.getArgument());
-    //                 // returnBasis.addBasis(sum);
-    //                 // returnBasis.addBasis(b2);
-    //                 matchingArguments = true;
-    //             }else{
-    //                 returnBasis.addBasis(subBasis);
-    //             }
-    //         }
-    //
-    //         if(! matchingArguments){
-    //             returnBasis.addBasis(b1);
-    //         }
-    //         return returnBasis;
-    //     }
-    //
-        Basis plusMultivariate(const Basis& b1, const MonomialeBasis& b2);
+    template< class BasisT >
+    Basis plusMultivariate(const Basis& b1, const BasisT b2){
+        Basis returnBasis = Basis();
+        bool matchingArguments = false;
 
+        for (Basis & subBasis : b1.getSubBasis()) {
+            if(subBasis.getArgument() == b2.getArgument()){
+                Basis sum = casted_plus_univariate(subBasis, b2);
+                sum.setArgument(subBasis.getArgument());
+                returnBasis.addBasis(sum);
+                matchingArguments = true;
+            }else{
+                returnBasis.addBasis(subBasis);
+            }
+        }
+        if(! matchingArguments){
+            returnBasis.addBasis(b2);
+        }
+        return returnBasis;
+    }
 
-        // Basis plusMultivariateMultivariate (const Basis& b1, const Basis& b2);
-        Basis plusMultivariate(const Basis& b1, const Basis& b2);
-    // #endif // SWIG
+    Basis plusMultivariate(const Basis& b1, const Basis& b2);
+
+    template< class BasisT >
+    Basis casted_plus_univariate(const Basis& b1, const BasisT b2){
+        BasisNode* w = b1.get();
+        Basis sum;
+        if(dynamic_cast<MonomialeBasisNode*>(w)){
+            sum = b2 + w->shared_from_this<MonomialeBasis>();
+        }
+        if(dynamic_cast<BSplineBasisNode*>(w)){
+            sum = b2 + w->shared_from_this<BSplineBasis>();
+        }
+        return  sum;
+    }
+
 } // namespace spline
 #endif //
