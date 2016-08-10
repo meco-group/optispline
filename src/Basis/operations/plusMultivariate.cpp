0404a38d1a83b53c795aa6056c2eae7391047f61
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-08-10 11:46:10 +0200

    operatorBasis

--- src/Basis/operations/plusMultivariate.cpp
+++ src/Basis/operations/plusMultivariate.cpp
@@ -4,18 +4,19 @@ namespace spline{
 
     Basis plusMultivariate(const Basis& b1, const Basis& b2){
         Basis returnBasis = b1;
-
         for (Basis & subBasis : b2.getSubBasis()) {
-            BasisNode* w = subBasis.get();
-            if(dynamic_cast<MonomialeBasisNode*>(w)){
-                returnBasis = returnBasis + w->shared_from_this<MonomialeBasis>();
-            }
-
-            if(dynamic_cast<BSplineBasisNode*>(w)){
-                returnBasis = returnBasis + w->shared_from_this<BSplineBasis>();
-            }
+           returnBasis = casted_plus_univariate(returnBasis, subBasis);
         }
-
         return returnBasis;
     }
+
+
+    Basis timesMultivariate(const Basis& b1, const Basis& b2){
+        Basis returnBasis = b1;
+        for (Basis & subBasis : b2.getSubBasis()) {
+           returnBasis = casted_plus_univariate(returnBasis, subBasis);
+        }
+        return returnBasis;
+    }
+
 }  // namespace spline
--- src/Basis/operations/plusMultivariate.h
+++ src/Basis/operations/plusMultivariate.h
@@ -9,40 +9,75 @@
 namespace spline{
 
     template< class BasisT >
-    Basis plusMultivariate(const Basis& b1, const BasisT b2){
-        Basis returnBasis = Basis();
-        bool matchingArguments = false;
+        Basis plusMultivariate(const Basis& b1, const BasisT b2){
+            Basis returnBasis = Basis();
+            bool matchingArguments = false;
 
-        for (Basis & subBasis : b1.getSubBasis()) {
-            if(subBasis.getArgument() == b2.getArgument()){
-                Basis sum = casted_plus_univariate(subBasis, b2);
-                sum.setArgument(subBasis.getArgument());
-                returnBasis.addBasis(sum);
-                matchingArguments = true;
-            }else{
-                returnBasis.addBasis(subBasis);
+            for (Basis & subBasis : b1.getSubBasis()) {
+                if(subBasis.getArgument() == b2.getArgument()){
+                    Basis sum = casted_plus_univariate(b2, subBasis);
+                    sum.setArgument(subBasis.getArgument());
+                    returnBasis.addBasis(sum);
+                    matchingArguments = true;
+                }else{
+                    returnBasis.addBasis(subBasis);
+                }
             }
+            if(! matchingArguments){
+                returnBasis.addBasis(b2);
+            }
+            return returnBasis;
         }
-        if(! matchingArguments){
-            returnBasis.addBasis(b2);
+
+ template< class BasisT >
+        Basis timesMultivariate(const Basis& b1, const BasisT b2){
+            Basis returnBasis = Basis();
+            bool matchingArguments = false;
+
+            for (Basis & subBasis : b1.getSubBasis()) {
+                if(subBasis.getArgument() == b2.getArgument()){
+                    Basis sum = casted_times_univariate(b2, subBasis);
+                    sum.setArgument(subBasis.getArgument());
+                    returnBasis.addBasis(sum);
+                    matchingArguments = true;
+                }else{
+                    returnBasis.addBasis(subBasis);
+                }
+            }
+            if(! matchingArguments){
+                returnBasis.addBasis(b2);
+            }
+            return returnBasis;
         }
-        return returnBasis;
-    }
 
     Basis plusMultivariate(const Basis& b1, const Basis& b2);
+    Basis timesMultivariate(const Basis& b1, const Basis& b2);
 
     template< class BasisT >
-    Basis casted_plus_univariate(const Basis& b1, const BasisT b2){
-        BasisNode* w = b1.get();
-        Basis sum;
-        if(dynamic_cast<MonomialeBasisNode*>(w)){
-            sum = b2 + w->shared_from_this<MonomialeBasis>();
+        Basis casted_plus_univariate(const BasisT b1, const Basis& b2){
+            BasisNode* w = b2.get();
+            Basis sum;
+            if(dynamic_cast<MonomialeBasisNode*>(w)){
+                sum = b1 + w->shared_from_this<MonomialeBasis>();
+            }
+            if(dynamic_cast<BSplineBasisNode*>(w)){
+                sum = b1 + w->shared_from_this<BSplineBasis>();
+            }
+            return sum;
         }
-        if(dynamic_cast<BSplineBasisNode*>(w)){
-            sum = b2 + w->shared_from_this<BSplineBasis>();
+
+    template< class BasisT >
+        Basis casted_times_univariate(const BasisT b1, const Basis& b2){
+            BasisNode* w = b2.get();
+            Basis product;
+            if(dynamic_cast<MonomialeBasisNode*>(w)){
+                product = b1 + w->shared_from_this<MonomialeBasis>();
+            }
+            if(dynamic_cast<BSplineBasisNode*>(w)){
+                product = b1 + w->shared_from_this<BSplineBasis>();
+            }
+            return product;
         }
-        return  sum;
-    }
 
 } // namespace spline
 #endif //
