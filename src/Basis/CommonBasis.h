8e63d767a9ad1753bfc65d2d319331b93364b665
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-09-21 11:20:53 +0200

    move utils

--- src/Basis/CommonBasis.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMMONBASIS_H
-#define COMMONBASIS_H
-
-typedef double AnyScalar;
-typedef DT AnyTensor;
-
-#endif /* COMMONBASIS_H */
--- src/Basis/operationsBasis.h
+++
@@ -1,8 +0,0 @@
-#ifndef OPERATIONSBASISG
-#define OPERATIONSBASISG
-
-#include "operations/operationsMultivariate.h"
-#include "operations/operationsUnivariate.h"
-
-#endif //  OPERATIONSBASISG
-
