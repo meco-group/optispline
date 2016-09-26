0e4e1f0db8e0dae4f3685683c7a7521ff1547a1e
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-09-26 13:26:11 +0200

    voor maarten

--- tests_maarten/test_basis_subBasis.py
+++ tests_maarten/test_basis_subBasis.py
@@ -1,8 +1,24 @@
 import unittest
 
+#!/usr/bin/env python
+import sys
+# sys.path.insert( 0, '/home/erik/Documents/cpp_splines/swig')
+sys.path.insert( 0, '../swig')
+sys.path.insert(0, '/home/erik/Documents/casadi3.0')
+import os
+
+# Boolean flag to indicate if we run in valgrind
+# To speed up valgrind, you may choose to put
+# expensive computations within 'if not valgrind'
+valgrind = int(os.environ.get("VALGRIND",'0'))
+
+from Basis import *
+from casadi import *
+
 class Test_Basis_SubBasis(unittest.TestCase):
 
     def test_upper(self):
+        
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
@@ -18,3 +34,5 @@ class Test_Basis_SubBasis(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    s = SubBasis()
+    print s
