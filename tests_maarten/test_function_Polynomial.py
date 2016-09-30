a3ebd3fbcb5c3e8b4513df02a8f0c3216411a4c3
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-09-30 10:42:45 +0200

    extra tests

---
+++ tests_maarten/test_function_Polynomial.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+
+# Boolean flag to indicate if we run in valgrind
+# To speed up valgrind, you may choose to put
+# expensive computations within 'if not valgrind'
+valgrind = int(os.environ.get("VALGRIND",'0'))
+
+import unittest
+from Basis import *
+from casadi import *
+
+class Test_Basis_SubBasis(unittest.TestCase):
+
+    def assertEqualTensor(self, a, b):
+        self.assertTrue(list(a.data().full())==b)
+
+    def assertNotEqualTensor(self, a, b):
+        self.assertFalse(list(a.data().full())==b)
+
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    a = [2,1,0]
+    p = Polynomial(a)
+    print p
+    print p([1]).data()
+    print p([3]).data()
+    
+    unittest.main()
