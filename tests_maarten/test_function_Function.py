13eceafcf4f7a2efd60200eb7dde667f8a94d5e0
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-10-03 15:46:08 +0200

    tests

--- tests_maarten/test_function_Function.py
+++ tests_maarten/test_function_Function.py
@@ -82,18 +82,15 @@ class Test_Basis_SubBasis(unittest.TestCase):
         self.assertEqual( f([2.0]).data(), 10 )
 
     def test_function_evaluation_2(self):
-        a1 = [1,1]
-        a2 = [0,2]
-        a_ = DTensor([a1,a2], [2,2,1,1])
+        a = DM([[1,1],[0,2]]) # 1 + y + 2xy
+        a_ = DTensor(a, [2,2,1,1])
         a_ = Coefficient(a_)
         m = SubMonomialBasis(1)
         b = Basis([m,m])
         f = Function(b,a_) 
-        print f([0.0,0.0]).data()
-        self.assertEqual( f([0.0,0.0]).data(), 2 )
-        self.assertEqual( f([1.0]).data(), 3 )
-        self.assertEqual( f([2.0]).data(), 10 )
-
+        for x in range(-5, 4):
+            for y in range(-5, 4):
+                self.assertEqual( f([x,y]).data() , 1 + y + 2*x*y)
 
 if __name__ == '__main__':
     unittest.main()
