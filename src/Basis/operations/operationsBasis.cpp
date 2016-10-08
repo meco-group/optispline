503ee84e0f9ab9ebe9bcc0b17bd9ba27a525ef4a
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-10-08 15:52:07 +0200

    plus on Basis

--- src/Basis/operations/operationsBasis.cpp
+++ src/Basis/operations/operationsBasis.cpp
@@ -1,14 +1,34 @@
 #include <vector>
+#include <algorithm>
 #include "operationsBasis.h"
 #include "../Basis.h"
 #include "../SubBasis.h"
+#include "../../Function/Argument.h"
 #include "../../common.h"	
 
 namespace spline{
-       Basis plusBasis(const Basis& b1,const  Basis& b2 ){
+
+    Basis plusBasis(const Basis& b1,const  Basis& b2 ){
         Basis returnBasis = Basis();
         if(b1.hasArguments() && b2.hasArguments()){
-            // std::vector<Argument> 
+            std::vector< Argument > allArguments = b1.getArguments();
+            for(auto &a : b2.getArguments()){
+                if(std::find(allArguments.begin(), allArguments.end(), a) == allArguments.end()) {
+                    /* v does not contain x */
+                    allArguments.push_back(a);
+                }         
+            } 
+
+            for(auto &a : allArguments){
+                SubBasis subBasis1 = b1.getSubBasis(a);
+                SubBasis subBasis2 = b2.getSubBasis(a);
+                std::cout << subBasis1.getRepresentation() << "   " << subBasis2.getRepresentation() << std::endl;
+                SubBasis sum = subBasis1 + subBasis2;
+                returnBasis.addBasis(sum);
+            }
+
+            returnBasis.setArguments(allArguments);
+
         }else{
             spline_assert_message(b1.getDimension() == b2.getDimension(),"Mismatch dimension");
             for(int i = 0; i < b1.getDimension(); i++){
--- tests/test_basis_operations_operationsBasis.py
+++ tests/test_basis_operations_operationsBasis.py
@@ -36,15 +36,35 @@ class Test_Basis_SubBasis(unittest.TestCase):
 if __name__ == '__main__':
     s1 = MonomialBasis(2)
     s2 = MonomialBasis(3)
-    s3 = BSplineBasis([0,1],2,2)
+    s3 = MonomialBasis(4)
+    s4 = BSplineBasis([0,1],2,2)
 
     b1 = Basis([s1,s2])
-    b2 = Basis([s2,s3])
+    b2 = Basis([s3,s4])
 
-    S1 = s1 + s2
-    S2 = s2 + s3
-
-
+    print "NO ARGUMENT"
     b = b1 + b2
+    print b
+    print b.getSubBasis()
 
+
+    x1 = Argument('x1')
+    x2 = Argument('x2')
+    x3 = Argument('x3')
+
+    b1.setArguments([x1,x2])
+    b2.setArguments([x2,x1])
+
+    print "SWAP"
+    b = b1 + b2
+    print b
+    print b.getSubBasis()
+
+    b3 = Basis([s3,s4])
+    b3.setArguments([x2,x3])
+
+    print "ARGUMENT COMBINE"
+    b = b1 + b3
+    print b
+    print b.getSubBasis()
     unittest.main()
