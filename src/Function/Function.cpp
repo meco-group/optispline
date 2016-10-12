f0d79c3327f638136b28ac4c460c7851f803748c
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-10-12 16:00:55 +0200

    function sum

--- src/Function/Function.cpp
+++ src/Function/Function.cpp
@@ -11,15 +11,30 @@ namespace spline {
     Function Function::operator+(Function f){
         Basis b = getBasis();
         EvaluationGrid evaluationGrid = EvaluationGrid(b);
+        std::vector< AnyTensor > basisEvaluated;
+        std::vector< AnyTensor > thisFunctionEvaluated;
+        std::vector< AnyTensor > otherFunctionEvaluated;
+        std::vector< AnyTensor > sumFunctionEvaluated;
 
-        // return Function(b,coef);
-        //
-        //     casadi::DM Univariatestd::shared_ptr<Basis>::transformation( const std::shared_ptr<Basis> &b) const {
-    //         std::vector<double> grid = evaluationGrid();
-    //         casadi::DM A(evalstd::shared_ptr<Basis>(grid));
-    //         casadi::DM B(b.evalstd::shared_ptr<Basis>(grid));
-    //         return casadi::DM::solve(A, B);
-//     }
+        evaluationGrid.evaluateEvaluationGrid(&basisEvaluated);
+        evaluationGrid.evaluateEvaluationGrid(&thisFunctionEvaluated, *this);
+        evaluationGrid.evaluateEvaluationGrid(&otherFunctionEvaluated, f);
+
+        for(int i = 0; i < basisEvaluated.size(); i++){
+            sumFunctionEvaluated.push_back(thisFunctionEvaluated[i] + otherFunctionEvaluated[i]);
+        }
+
+
+
+
+            // return Function(b,coef);
+            //
+            //     casadi::DM Univariatestd::shared_ptr<Basis>::transformation( const std::shared_ptr<Basis> &b) const {
+            //         std::vector<double> grid = evaluationGrid();
+            //         casadi::DM A(evalstd::shared_ptr<Basis>(grid));
+            //         casadi::DM B(b.evalstd::shared_ptr<Basis>(grid));
+            //         return casadi::DM::solve(A, B);
+            //     }
     }
 
 }  // namespace spline
