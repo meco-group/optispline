de5cf9f971844b8bbe9cd204124f677bbe121fa7
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2016-10-13 10:33:49 +0200

    Polynomial extra constructors

--- src/Function/Polynomial.cpp
+++ src/Function/Polynomial.cpp
@@ -8,5 +8,15 @@ namespace spline{
         Function(*(new MonomialBasis(coef.size()-1)), Coefficient(coef)) {
     }
 
+    Polynomial::Polynomial(const std::vector< double >& coef, Argument a) :
+        Function(*(new MonomialBasis(coef.size()-1)), Coefficient(coef)) {
+        getBasis().setArguments(std::vector< Argument > {a});
+    }
+
+    Polynomial::Polynomial(const std::vector< double >& coef, const std::string &name) :
+        Function(*(new MonomialBasis(coef.size()-1)), Coefficient(coef)) {
+        std::vector< Argument > argumentList = {*(new Argument(name))};
+        getBasis().setArguments(argumentList);
+    }
 
 } // namespace spline
--- src/Function/Polynomial.h
+++ src/Function/Polynomial.h
@@ -10,7 +10,10 @@ namespace spline{
 class Polynomial : public Function {
 public:
     Polynomial(const std::vector< double >& coef);
+    Polynomial(const std::vector< double >& coef, Argument a);
+    Polynomial(const std::vector< double >& coef, const std::string &name);
     // virtual std::string getRepresentation() const ;
 };
 } // namespace spline
 #endif /* POLYNOMIAL_H */
+
