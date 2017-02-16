aa28b83ea18eb15da47ade4a4df941606a68b6df
rubenvanparys <ruben.vanparys@kuleuven.be>
2017-02-16 07:52:17 +0100

    updated test on basis construction with symbolic knots

--- tests/test_basis_BSplineBasis.py
+++ tests/test_basis_BSplineBasis.py
@@ -13,7 +13,7 @@ class Test_Basis_BSplineBasis(BasisTestCase):
     def test_greville(self):
         degree = 3
         knotsint = 8
-        knots = np.r_[np.zeros(degree),np.linspace(0.,1.,knotsint),np.ones(degree)]
+        knots = np.r_[np.zeros(degree),np.linspace(0., 1., knotsint), np.ones(degree)]
         dim_b = knots.shape[0] - degree - 1
         g = np.zeros(dim_b);
         for i in range(dim_b):
@@ -28,12 +28,20 @@ class Test_Basis_BSplineBasis(BasisTestCase):
     def test_constructor(self):
         degree = 3
         knotsint = 8
-        knots = np.r_[np.zeros(degree),np.linspace(0.,1.,knotsint),np.ones(degree)]
-        b = BSplineBasis(np.flipud(knots),degree)
-        # self.assertEqualT(knots,b.getKnots) # test if sorted
+        knots = np.r_[np.zeros(degree),np.linspace(0., 1., knotsint), np.ones(degree)]
+        b = BSplineBasis(np.flipud(knots), degree)
+        self.assertEqualT(knots, b.getKnots()) # test if sorted
         with self.assertRaises(Exception):
-            b = BSplineBasis(np.vstack(knots,knots),degree)
-            b = BSplineBasis(np.c_[knots],degree)
+            b = BSplineBasis(np.c_[knots], degree)
+            b = BSplineBasis(np.vstack(knots,knots), degree)
+        # with symbolic knots
+        sym_k1 = MX.sym('knot1')
+        sym_k2 = MX.sym('knot2')
+        knots1 = knots.tolist() + [sym_k1, sym_k2]
+        knots2 = knots.tolist() + [0.8, 0.55]
+        b = BSplineBasis(knots1, degree)
+        kn = casadi.Function('f', [sym_k1, sym_k2], b.getKnots())
+        self.assertEqualT(np.sort(knots2), kn(0.8, 0.55))
 
 # TODO constructor
     # def test_getEvaluation1(self):
