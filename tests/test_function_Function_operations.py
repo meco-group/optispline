309df572eac7ca9bdf1955343585147ce007db50
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-02-16 09:14:31 +0100

    function operation unit test

--- tests/test_function_Function_operations.py
+++ tests/test_function_Function_operations.py
@@ -49,38 +49,45 @@ class Test_Function_Operations(BasisTestCase):
         c = Function.Constant(basis,1)
         self.assertEqualT(c(0.5),DTensor(1))
 
-#    def test_bspline_operation(self):
-#        knots1 = [0,0,0.4,1,1]
-#        degree = 1
-#        basis1 = Basis.BSplineBasis(knots1,degree)
-#
-#        knots2 = [0.2,0.2,0.5,1,1]
-#        degree = 1
-#        basis2 = Basis.BSplineBasis(knots2,degree)
-#
-#        coeff1 = DTensor(numpy.random.randn(3,1),[3,1,1])
-#        coeff2 = DTensor(numpy.random.randn(3,1),[3,1,1])
-#        func1 = Function(basis1,coeff1)
-#        func2 = Function(basis2,coeff2)
-#
-#        # function evaluations
-#        x = [-0.1,0.1,0.35,0.4,0.5,0.8,0.99,1.2,1.4] #range(-1.,2.1,10) 
-#        func1_value = []
-#        func2_value = []
-#        for v in x:
-#            func1_value.append(func1(v))
-#            func2_value.append(func2(v))
-#
-#        # function operations
-#        u1 = -func1
-#        #s1 = func1 + 2.0
-#        s3 = func1 + func1
-#        s2 = func1 + func2
-#        #d1 = func2 - 5.0
-#        d2 = func2 - func1
-#        #m1 = func1*2.0
-#        m2 = func1*func2
-#        #p = func1**3
+        c2 = Function.Constant(basis,DTensor([2,2],[1,2]))
+        self.assertEqualT(c2(0.5),DTensor([2,2],[1,2]))
+        
+        basis2 = Basis.MonomialBasis(3)
+        c3 = Function.Constant(basis2,1)
+        self.assertEqualT(c3(0.5),DTensor(1))
+
+    def test_bspline_operation(self):
+        knots1 = [0,0,0.4,1,1]
+        degree = 1
+        basis1 = Basis.BSplineBasis(knots1,degree)
+
+        knots2 = [0.2,0.2,0.5,1,1]
+        degree = 1
+        basis2 = Basis.BSplineBasis(knots2,degree)
+
+        coeff1 = DTensor(numpy.random.randn(3,1),[3,1,1])
+        coeff2 = DTensor(numpy.random.randn(3,1),[3,1,1])
+        func1 = Function(basis1,coeff1)
+        func2 = Function(basis2,coeff2)
+
+        # function evaluations
+        x = [-0.1,0.1,0.35,0.4,0.5,0.8,0.99,1.2,1.4] #range(-1.,2.1,10) 
+        func1_value = []
+        func2_value = []
+        for v in x:
+            func1_value.append(func1(v))
+            func2_value.append(func2(v))
+
+        # function operations
+        u1 = -func1
+        #s1 = func1 + 2.0
+        s3 = func1 + func1
+        s2 = func1 + func2
+        #d1 = func2 - 5.0
+        d2 = func2 - func1
+        #m1 = func1*2.0
+        m2 = func1*func2
+        #p = func1**3
 #
 #        # assertion over grid
 #        k = 0
