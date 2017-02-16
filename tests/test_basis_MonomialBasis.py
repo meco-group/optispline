bb9f3663893af88829bb4a3161b3648fcf57fe9f
rubenvanparys <ruben.vanparys@kuleuven.be>
2017-02-16 12:40:21 +0100

    deactivate derivate test

--- tests/test_basis_MonomialBasis.py
+++ tests/test_basis_MonomialBasis.py
@@ -12,20 +12,20 @@ class Test_Basis_MonomialBasis(BasisTestCase):
 
     def test_derivative(self):
         return
-        degree = 4
-        n_der = 1
-        b = MonomialBasis(degree)
-        db,T = b.derivative(n_der)
-        x = casadi.SX.sym( 'x')
-        db_c = b([x])
-        for i in range(0,n_der):
-            db_c = casadi.jacobian(db_c, x)
-        db_c = casadi.Function('db_c', [x], [db_c] )
+        # degree = 4
+        # n_der = 1
+        # b = MonomialBasis(degree)
+        # db,T = b.derivative(n_der)
+        # x = casadi.SX.sym( 'x')
+        # db_c = b([x])
+        # for i in range(0,n_der):
+        #     db_c = casadi.jacobian(db_c, x)
+        # db_c = casadi.Function('db_c', [x], [db_c] )
 
-        np.random.seed(0)
-        g = -5 + 10 * np.random.rand(4)
-        for i in g
-            self.assertEqualT(db_c(i), db(i))
+        # np.random.seed(0)
+        # g = -5 + 10 * np.random.rand(4)
+        # for i in g
+        #     self.assertEqualT(db_c(i), db(i))
 
 # TODO constructor
     # def test_getEvaluation1(self):
