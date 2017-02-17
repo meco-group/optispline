3bcdaa3beda2935e51b3ca9a969e13ec01bcc82f
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-02-17 09:41:41 +0100

    extra fixes for anyvector

--- tensor-tools/any_tensor.cpp
+++ tensor-tools/any_tensor.cpp
@@ -379,21 +379,21 @@ std::vector<AnyScalar> AnyScalar::from_vector(const std::vector<MX>& v) {
 }
 
 AnyVector::AnyVector(const AnyTensor& s) : AnyTensor(s.squeeze()) {
-  tensor_assert_message(n_dims()==1, "AnyVector can have only one dimension. Got " << s.dims() << ".")
+  tensor_assert_message(n_dims()<=1, "AnyVector can have only one dimension. Got " << s.dims() << ".")
 }
 
 AnyVector::AnyVector(const std::vector<AnyScalar>& s) : AnyTensor(vertcat(s)) { }
 
 AnyVector::AnyVector(const DT & s) : AnyTensor(s.squeeze()){
-  tensor_assert_message(n_dims()==1, "AnyVector can have only one dimension.")
+  tensor_assert_message(n_dims()<=1, "AnyVector can have only one dimension.")
 }
 
 AnyVector::AnyVector(const ST & s) : AnyTensor(s.squeeze()){
-  tensor_assert_message(n_dims()==1, "AnyVector can have only one dimension.")
+  tensor_assert_message(n_dims()<=1, "AnyVector can have only one dimension.")
 }
 
 AnyVector::AnyVector(const MT & s) : AnyTensor(s.squeeze()){
-  tensor_assert_message(n_dims()==1, "AnyVector can have only one dimension.")
+  tensor_assert_message(n_dims()<=1, "AnyVector can have only one dimension.")
 }
 
 /**AnyVector& AnyVector::operator=(const AnyTensor& s)  {
