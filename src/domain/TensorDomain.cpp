2016bb5ae11ca552f937c8aafdff374018566d35
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2017-02-20 18:41:21 +0100

    TensorDomain

---
+++ src/domain/TensorDomain.cpp
@@ -0,0 +1,46 @@
+#include "TensorDomain.h"
+#include "Domain.h"
+#include "../common.h"
+
+namespace spline {
+
+    TensorDomainNode* TensorDomain::get() const {
+        return static_cast<TensorDomainNode*>(SharedObject::get());
+    }
+
+    TensorDomainNode* TensorDomain::operator->() const { return get(); }
+
+    TensorDomain::TensorDomain() {
+        assign_node(new TensorDomainNode(std::vector< Domain >{}));
+    }
+
+    TensorDomain::TensorDomain(const Domain & allSubDomain) {
+        assign_node(new TensorDomainNode(std::vector< Domain > {allSubDomain}));
+    }
+
+    TensorDomain::TensorDomain(const std::vector< Domain >& allSubDomain) {
+        assign_node(new TensorDomainNode(allSubDomain));
+    }
+
+    int TensorDomain::n_domains() const { return (*this)->dimension ();}
+    int TensorDomainNode::n_domains() const {
+        return allDomains.size();
+    }
+
+    /* std::string TensorDomainNode::getRepresentation() const { */
+    /*     return "TensorDomain"  + std::to_string(allArguments.size()) + */
+    /*         std::to_string(allSubDomain.size()); */
+    /* } */
+
+    /* std::string TensorDomain::getRepresentation() const { return (*this)->getRepresentation() ;} */
+    /* std::ostream& operator<<(std::ostream &stream, const TensorDomain& base) { */
+    /*     return stream << base.getRepresentation(); */
+    /* } */
+
+    TensorDomain TensorDomain::intersection() const { return (*this)->dimension ();}
+    TensorDomain TensorDomainNode::intersection() const {
+        spline_assert_message(false, "test lengtes en zo");
+        return TensorDomain();
+    }
+
+} // namespace spline
---
+++ src/domain/TensorDomain.h
@@ -0,0 +1,50 @@
+#ifndef SRC_DOMAIN_TENSORDOMAIN_H_
+#define SRC_DOMAIN_TENSORDOMAIN_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <any_tensor.hpp>
+
+#include "../SharedObject/SharedObject.h"
+#include "../SharedObject/SharedObjectNode.h"
+
+#include "../Function/Argument.h"
+#include "../Function/Index.h"
+#include "../Function/NumericIndex.h"
+
+namespace spline {
+
+class TensorDomain;
+class Domain;
+
+#ifndef SWIG
+
+class TensorDomainNode : public SharedObjectNode {
+    public:
+        int n_domains() const;
+        TensorDomain intersection() const;
+    private:
+        std::vector< Domain > allDomains;
+};
+
+#endif // SWIG
+
+class TensorDomain : public SharedObject{
+public:
+#ifndef SWIG
+
+        TensorDomainNode* get() const;
+        TensorDomainNode* operator->() const;
+
+#endif // SWIG
+        TensorDomain();
+        TensorDomain(const Domain & allSubDomain);
+        TensorDomain::TensorDomain(const std::vector< Domain >& allSubDomain);
+
+        int n_domains() const; // Vector containing number of basis functions in each basi
+        TensorDomain intersection() const;
+
+}   // namespace spline
+
+#endif  // SRC_DOMAIN_TENSORDOMAIN_H_
