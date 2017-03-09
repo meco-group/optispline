fe3d955b40ea263efd1897642f6a0e3831d025fc
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2017-03-09 14:02:56 +0100

    constant eval

--- src/Function/ConstantNode.cpp
+++ src/Function/ConstantNode.cpp
@@ -11,7 +11,8 @@ namespace spline {
     }
 
     AnyTensor ConstantNode::operator()(const AnyTensor& x, const std::vector< std::string >& args) const{
-        spline_assert_message(false, "not implemented: evaluation constant");
+        return coeff_tensor();
+        /* spline_assert_message(false, "not implemented: evaluation constant"); */
         /* return packed_tensor.einstein(coeff().data(), a_r, b_r, c_r).squeeze(); */
     }
 
