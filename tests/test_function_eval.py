6231661203cce85d0a7d5bd8d5aa97b2c6722e0e
u0098366 <tim.mercy@kuleuven.be>
2017-03-17 15:30:29 +0100

    Added f_partial test

--- tests/test_function_eval.py
+++ tests/test_function_eval.py
@@ -40,6 +40,30 @@ class Test_Function_Operations(BasisTestCase):
       print f(r2)
       print f(r2, ['x'])
 
+    def test_partial_eval(self):
+        return
+        numpy.random.seed(0)
+
+        knots1 = [0,0,0,0,0.2,0.4,0.6,0.8,1,1,1,1]
+        degree = 3
+        basis1 = splines.BSplineBasis(knots1,degree)
+
+        knots2 = [0.,0.,0.5,1,1]
+        degree = 1
+        basis2 = splines.BSplineBasis(knots2,degree)
+
+        mbasis1 = TensorBasis([basis1,basis2], ['x','y']);
+        coeff1 = DTensor(numpy.random.randn(18,1),[6,3,1,1])
+        func1 = Function(mbasis1,coeff1)
+
+        f_part1 = func1.f_partial(10, 'x')
+        f_part2 = f_part1(4)
+
+        f_part3 = func1.f_partial(4, 'y')
+        f_part4 = f_part1(10)
+
+        self.assertEqualT(f_part2, f_part4)
+        self.assertEqualT(f_part4, func1([10, 4], ['x','y']))
 
 if __name__ == '__main__':
     unittest.main()
