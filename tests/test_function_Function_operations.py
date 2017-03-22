2a4a0df4c82efabb8f5ecd687a2347e4209906cd
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2017-03-22 18:18:13 +0100

    nesesari function test

--- tests/test_function_Function_operations.py
+++ tests/test_function_Function_operations.py
@@ -19,34 +19,35 @@ class Test_Function_Operations(BasisTestCase):
       p2 = Polynomial([0,1],'y')
 
       x = Polynomial([0,1],'x')
-      y = p2
+      y = Polynomial([0,1],'y')
       # f = x + y
       # f = y*x
-      # f = x + y*x + y*y
+      f = x + y*x + y*y
 
-#
-#      # for p,poly in [
-#      #                 (p1+p2, lambda x,y: x**2+y),
-#      #                 (p1*p2, lambda x,y: x**2*y),
-#      #                 (f, lambda x,y: x + y*x + y*y),
-#      #                 (f + p1+p2, lambda x,y: x + y*x + y*y+x**2+y),
-#      #                 (f + p1*p2, lambda x,y: x + y*x + y*y+x**2*y)
-#      #               ]:
-#
-#      #   for arg in [ [0,0], [1,0], [0,1], [2,2], [0.7,1.3]]:
-#      #     self.assertEqualTensor(p(*arg), poly(*arg))
-#
-#
-#      # p1 = Polynomial([0,0,1],'x')
-#      # p2 = Polynomial([0,1])
-#
-#      # for p,poly in [
-#      #                 (p1+p2, lambda x: x**2+x),
-#      #                 (p1*p2, lambda x: x**3),
-#      #               ]:
-#
-#      #   for arg in [0,1,2,1.3]:
-#      #     self.assertEqualTensor(p(arg), poly(arg))
+
+      for p,poly in [
+                     (p1+p2, lambda x,y: x**2+y),
+                     (p1*p2, lambda x,y: x**2*y),
+                     (f, lambda x,y: x + y*x + y*y),
+                     (f + p1+p2, lambda x,y: x + y*x + y*y+x**2+y),
+                     (f + p1*p2, lambda x,y: x + y*x + y*y+x**2*y)
+                   ]:
+          for arg in [ [0,0], [1,0], [0,1], [2,2], [0.5,1.1]]:
+              b = p.tensor_basis().basis_functions()
+              self.assertEqualTensor(p(arg, ['x', 'y']), poly(*arg))
+              self.assertEqualTensor(p(*arg), poly(*arg))
+
+
+      p1 = Polynomial([0,0,1],'x')
+      p2 = Polynomial([0,1])
+
+      for p,poly in [
+                      (p1+p2, lambda x: x**2+x),
+                      (p1*p2, lambda x: x**3),
+                    ]:
+
+        for arg in [0,1,2,1.3]:
+          self.assertEqualTensor(p(arg), poly(arg))
 #        x = Polynomial([0,1],'x')
 #        y = p2
 #        f = x + y
