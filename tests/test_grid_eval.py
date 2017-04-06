bb43e90915274550036f05311c1f636372d29794
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2017-04-06 18:02:38 +0200

    tests

--- tests/test_grid_eval.py
+++ tests/test_grid_eval.py
@@ -13,6 +13,15 @@ class Test_grid_eval(BasisTestCase):
         b = b.add_basis(s1)
         self.assertEqual(len(b.bases()), 1)
 
+    def test_function(self):
+        x = Polynomial([0, 1], 'x')
+        y = Polynomial([0, 1], 'y')
+
+        r = range(3)
+
+        print r
+        print x.grid_eval([r],['x'])
+
 if __name__ == '__main__':
     unittest.main()
 
