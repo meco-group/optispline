ef84a5c278c9502ac6258c1b09f71375175a32f6
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2017-04-07 17:14:34 +0200

    update tests

--- tests/test_grid_eval.py
+++ tests/test_grid_eval.py
@@ -28,18 +28,22 @@ class Test_grid_eval(BasisTestCase):
             for j in range(len(r2)):
                 self.assertAlmostEqual(f(r1[i], r2[j]) ,grid_e[i,j])
 
-        tb = TensorBasis([BSplineBasis([0,1],1,7), BSplineBasis([0,1.1],3,3)])
+        tb = TensorBasis([BSplineBasis([0,1],1,7), BSplineBasis([0,1.1],3,3)], ['x', 'y'])
         c = np.random.rand(*tb.dimension())
         f  = Function(tb, c)
 
-        r1 = np.random.rand(5)
+        r1 = np.random.rand(2)
         r2 = np.random.rand(5)
 
         grid_e = f.grid_eval([r1  ,r2],['x', 'y'])
-
         for i in range(len(r1)):
             for j in range(len(r2)):
                 self.assertAlmostEqual(f([r1[i], r2[j]],['x','y']) ,grid_e[i,j])
+
+        grid_e = f.grid_eval([r2  ,r1],['y', 'x'])
+        for i in range(len(r1)):
+            for j in range(len(r2)):
+                self.assertAlmostEqual(f([r1[i], r2[j]],['x','y']) ,grid_e[j,i])
 
 if __name__ == '__main__':
     unittest.main()
