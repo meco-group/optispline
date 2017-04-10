92662590466b7bba568ec6d7aa2d0528eb800cfb
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2017-04-10 10:21:06 +0200

    update tests

--- tests/test_grid_eval.py
+++ tests/test_grid_eval.py
@@ -13,6 +13,37 @@ class Test_grid_eval(BasisTestCase):
         b = b.add_basis(s1)
         self.assertEqual(len(b.bases()), 1)
 
+    def test_basis(self):
+        x = MonomialBasis(2)
+        y = MonomialBasis(1)
+
+        tb = TensorBasis([x,y], ['x', 'y'])
+
+        r1 = range(3)
+        r2 = range(4)
+
+        grid_e = tb.grid_eval([r1  ,r2],['x', 'y'])
+
+        for i in range(len(r1)):
+            for j in range(len(r2)):
+                self.assertEqualTensor(tb([r1[i], r2[j]]) ,grid_e[i,j])
+
+        tb = TensorBasis([BSplineBasis([0,1],1,4), BSplineBasis([0,1.1],2,2)], ['x', 'y'])
+
+        r1 = np.random.rand(2)
+        r2 = np.random.rand(5)
+
+        grid_e = tb.grid_eval([r1  ,r2],['x', 'y'])
+        for i in range(len(r1)):
+            for j in range(len(r2)):
+                self.assertEqualTensor(tb([r1[i], r2[j]],['x','y']) ,grid_e[i,j])
+
+        grid_e = tb.grid_eval([r2  ,r1],['y', 'x'])
+        for i in range(len(r1)):
+            for j in range(len(r2)):
+                self.assertEqualTensor(tb([r2[j], r1[i]],['y','x']),grid_e[j,i])
+
+
     def test_function(self):
         x = Polynomial([0, 1], 'x')
         y = Polynomial([0, 1], 'y')
