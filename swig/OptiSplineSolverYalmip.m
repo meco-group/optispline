ac6ba24c6d55d84f8309ed32081619342c05b77d
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-04-13 00:04:31 +0200

    fixup! bugfix optistack: problem when variables have different shapes

--- swig/OptiSplineSolverYalmip.m
+++ swig/OptiSplineSolverYalmip.m
@@ -38,12 +38,8 @@ classdef OptiSplineSolverYalmip < splines.OptiSplineSolver
         if isempty(c)
           c = zeros(0,1);
         end
-        
-        p = vertcat(vars_p_yalmip{:});
-        if isempty(p)
-          p = zeros(0,1);
-        end
-        sol = optimizer(c, opti.yalmip_expr(f), yalmip_options, p, vertcat(vars_x_yalmip{:}));
+
+        sol = optimizer(c, opti.yalmip_expr(f), yalmip_options, vertcat(vars_p_yalmip{:}), vertcat(vars_x_yalmip{:}));
         self.sol = sol;
 
       end
--- tests/test_optistack.m
+++ tests/test_optistack.m
@@ -117,6 +117,8 @@ F = opti.Function(b,[4,4]);
 F = opti.Function(b,[4,4],'full');
 F = opti.Function(b,[4,4],'symmetric');
 
+opti = OptiSplineYalmip();
+
 
 
 x = opti.var();
@@ -124,7 +126,7 @@ y = opti.var(4,4);
 z = opti.var(1,3);
 
 123
-sol = opti.solver(x+trace(y)+z(1),{},'yalmip',struct('yalmip_options',yalmip_options))
+sol = opti.solver(x+trace(y)+z(1),{y>=3},'yalmip')
 sol.solve()
 
 
