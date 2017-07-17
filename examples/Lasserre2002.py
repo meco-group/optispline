3c8a975486df567d184c8c552219e1b02f9166d6
Erik Lambrechts; u0078550 <erik.lambrechts@kuleuven.be>
2017-07-17 20:32:27 +0200

    2 examples

--- examples/Lasserre2002.py
+++ examples/Lasserre2002.py
@@ -9,20 +9,80 @@ b = BSplineBasis([0, 1], 1, 2)
 x = Function(b, [0, 1])
 f = x * ( x-1 )
 print f
+
+# for i in range(14):
+#     f_ = f.degree_elevation(i)
+#     print i ,
+#     print " : ",
+#     print len(f_.coeff_tensor().flatten()) ,
+#     print " : ",
+#     print min((f_.coeff_tensor().flatten()))
+
+# for i in range(2):
+#     f_ = f.insert_knots([(1.0+j)/(i+1) for j in range(i)])
+#     print i ,
+#     print " : ",
+#     print len(f_.coeff_tensor().flatten()) ,
+#     print " : ",
+#     print min((f_.coeff_tensor().flatten()))
+
+# print "--------------------"
+
+x = Function(b, [0, 1])
+f = ( x**2-0.5 )**2
+print f
 print f.coeff_tensor()
 
-for i in range(15):
+polya = []
+bspline = []
+r = range(0,13)
+r = [0,1,3,6,10]
+for i in r:
     f_ = f.degree_elevation(i)
     print i ,
     print " : ",
     print len(f_.coeff_tensor().flatten()) ,
     print " : ",
     print min((f_.coeff_tensor().flatten()))
+    polya.append(min(f_.coeff_tensor().flatten()))
 
-for i in range(2):
+for i in r:
     f_ = f.insert_knots([(1.0+j)/(i+1) for j in range(i)])
     print i ,
     print " : ",
     print len(f_.coeff_tensor().flatten()) ,
     print " : ",
     print min((f_.coeff_tensor().flatten()))
+    bspline.append(min(f_.coeff_tensor().flatten()))
+
+import numpy as np
+import matplotlib.pyplot as plt
+# Just a figure and one subplot
+fig, ax = plt.subplots()
+# print r
+# print polya
+# ax.plot(r, np.log(-np.array(polya)),'--', r, np.log(-np.array(bspline)),"-")
+ax.plot(r, polya,'--', r, bspline,"-")
+ax.plot(r, [0 for _ in r])
+# ax.set_yscale('symlog')
+plt.show()
+
+
+
+# sq2 = np.sqrt(0.5)
+# n=2
+# f_ = f.insert_knots([-.05+ sq2 + (- n/2.0 + j )/20 for j in range(n)])
+# f_ = f.insert_knots([0.51, 0.73])
+# # f_ = f.insert_knots([sq2 - 0.2, sq2 - 0.1, sq2, sq2+0.1, sq2+0.2])
+# print len(f_.coeff_tensor().flatten()) ,
+# print " : ",
+# print min((f_.coeff_tensor().flatten()))
+# bspline.append(min(f_.coeff_tensor().flatten()))
+# # print f_.basis().greville()
+
+# ax.plot(f_.basis().greville(), f_.coeff_tensor().flatten())
+
+# n = 100
+# t = [1.0* i/n for i in range(n+1)]
+# ax.plot(t,f.list_eval(t))
+# # ax.show()
---
+++ examples/circle.py
@@ -0,0 +1,58 @@
+import meco_binaries
+meco_binaries(cpp_splines="")
+from splines import *
+import numpy as np
+import time
+
+pi = np.pi
+knots = [0,0,0,1,1,1]
+
+sq05 = np.sqrt(0.5)
+weights = DTensor([1, sq05, 1], [3])
+
+b = BSplineBasis(knots, 2)
+
+coef_cos_1 = sq05*DTensor([ 1,np.sqrt(2),1], [3])
+coef_sin_1 = sq05*DTensor([ -1, 0, 1 ], [3])
+
+cos1 = Function(TensorBasis(b, "alpha") , coef_cos_1)
+sin1 = Function(TensorBasis(b, "alpha") , coef_sin_1)
+
+noemer = Function(TensorBasis(b, "alpha") , weights)
+
+no = noemer**2
+s1 = sin1*noemer * np.sqrt(2)
+c1 = cos1*noemer * np.sqrt(2) - no
+s2 = 2*sin1*cos1
+c2 = cos1**2 - sin1**2
+
+
+n = 100
+t = [((1.0*i)/(n)) for i in range(n+1)]
+
+s = Function.horzcat([s1, s2])
+c = Function.horzcat([c1, c2])
+
+B = TensorBasis([b*b, BSplineBasis([0,1],1,2)], ['a', 'r'])
+s = Function(B, s.coeff_tensor().squeeze())
+c = Function(B, c.coeff_tensor().squeeze())
+
+# def f(x,y):
+#     return [(c([x,y],['a','r'])/no(x)), (s([x,y],['a','r'])/no(x))]
+
+# import matplotlib.pyplot as plt
+# t = [1.0*i/(n) for i in range(n+1)]
+# for j in np.linspace(0,1,6):
+#     p = [ f(i,j) for i in t]
+#     p = np.array(p)
+#     plt.plot(p[:,0],p[:,1])
+#     p = [ f(j,i) for i in t]
+#     p = np.array(p)
+#     plt.plot(p[:,0],p[:,1])
+# plt.show()
+
+
+p = (c - 0.5*no)**2 + (s - 0.5*no)**2
+print min(p.coeff_tensor().flatten())
+for i in range(7):
+    print min(p.midpoint_refinement(i).coeff_tensor().flatten())
