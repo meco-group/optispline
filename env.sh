8dd60663b1eae9ad4d8a6b7a2211c24a37274f92
Joris Gillis <joris.gillis42gmail.com>
2018-03-20 14:50:19 +0100

    env

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@ if [ -z "$BUILD_LANG" ]; then
     echo "You must set BUILD_LANG to python27 or matlabR2014a"
 fi
 
-export CASADIVERSION=3.3.0-rc2
+export CASADIVERSION=3.4.0
 export SWIG_HOME_REL=swig/2.0
 export SWIG_HOME=$DEBIAN_BINARIES/$SWIG_HOME_REL
 
@@ -12,5 +12,5 @@ export PATH=$SWIG_HOME/bin:$SWIG_HOME/share:$PATH
 export CASADI_PREFIX_REL=$BUILD_LANG/casadi/$CASADIVERSION
 
 export CASADI_PREFIX=$DEBIAN_BINARIES/$CASADI_PREFIX_REL
-export CC="gcc-4.7"
-export CXX="g++-4.7"
+#export CC="gcc-4.7"
+#export CXX="g++-4.7"
