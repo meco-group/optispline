f95ddcb99982edd7971f5bb13c8f157c21c95de9
Joris Gillis <joris.gillis@kuleuven.be>
2019-05-07 11:52:37 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@ if [ -z "$BUILD_LANG" ]; then
     echo "You must set BUILD_LANG to python27 or matlabR2014a"
 fi
 
-export CASADIVERSION=f7df306
+export CASADIVERSION=d84570c
 export SWIG_HOME_REL=swig/2.0
 export SWIG_HOME=$DEBIAN_BINARIES/$SWIG_HOME_REL
 
